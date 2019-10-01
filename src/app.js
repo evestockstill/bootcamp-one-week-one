556058cbc88c21317c524fe24b53f98116260a2f
Eve Stockstill <evestockstillpdx@gmail.com>
2019-10-01 01:21:03 -0700

    working prompt

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 import isYes from '../src/isYes.js';
 
 const quizbtn = document.getElementById('quizbtn');
-// quizbtn.addEventListener('click');
+const correctAnswers = document.getElementById('correct');
 
-    
+
+let correct = 0;
 
 quizbtn.onclick = () => {
-    alert('Hello, thanks for taking the quiz');
     
+    const userName = prompt('What is your name?');
+    alert(`Hello ${userName} do you want to take my quiz?`);
+
     const answer1 = prompt('Do I like to play pool?');
     if (isYes(answer1) === true) {
+        correct++;
+        correctAnswers.textContent = correct;
         alert('Yes I do!');
     }
     else {
@@ -17,15 +22,22 @@ quizbtn.onclick = () => {
     }
     const answer2 = prompt('Do I love my Dog?');
     if (isYes(answer2) === true) {
+        correct++;
+        correctAnswers.textContent = correct;
         alert('Correct!');
     } else {
         alert('guess again');
     }
     const answer3 = prompt('Do I enjoy camping?');
     if (isYes(answer3) === true) {
+       
+        correct++;
+        correctAnswers.textContent = correct;
         alert('Of Course');
-    }
-    else {
+        const quizAnswers = document.getElementById('correct');
+        quizAnswers.classList.remove('hidden');
+        
+    } else {
         alert('try again');
     }
 };
